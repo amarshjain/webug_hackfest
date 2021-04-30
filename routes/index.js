6bb94574c38193b391f6f3bf6f4d951ef55ecee8
mmaj5524 <amarshjain777@gmail.com>
2021-05-01 01:43:01 +0530

    post method

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 // index GET route
 router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {vulns: []});
+});
+
+router.post("/", (req, res) => {
+    
+    try {
+
+        const scanUri = req.body.scanUri;
+
+        if(scanUri === ''){
+            res.redirect("/index");
+            return;
+        }
+        var vulns = [];
+        
+        res.render("index", {vulns: vulns});
+
+    } catch (err) {
+
+        console.log(err);
+
+    }
+
 });
 
 module.exports = router;
